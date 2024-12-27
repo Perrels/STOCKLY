3bb1bfeb8349478775762dcb93171cf6ad32654f
Perrels <pablorela@gmail.com>
2024-12-27 00:46:10 -0300

    Feat add alert dialog to delete product button

--- src/app/(dashboard)/products/_components/table-columns.tsx
+++ src/app/(dashboard)/products/_components/table-columns.tsx
@@ -9,10 +9,20 @@ import {
   ClipboardCopyIcon,
   EditIcon,
   Info,
-  MoreHorizontalIcon,
   MoreVerticalIcon,
   TrashIcon,
 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/app/_components/ui/alert-dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -82,28 +92,53 @@ export const productTableColumns: ColumnDef<Product>[] = [
       const product = row.row.original;
       const idToString = product.id.toString();
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost">
-              <MoreVerticalIcon className="w-4 h-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>Ações</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="gap-1.5"
-              onClick={() => {
-                navigator.clipboard.writeText(idToString);
-              }}
-            >
-              <ClipboardCopyIcon size={16} /> Copiar ID
-            </DropdownMenuItem>
-            <DropdownMenuItem className="gap-1.5"><EditIcon size={16}/> Editar</DropdownMenuItem>
-            <DropdownMenuItem className="gap-1.5"><TrashIcon size={16}/>Excluir</DropdownMenuItem>
-            <DropdownMenuItem className="gap-1.5"><Info size={16}/> Detalhes</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <AlertDialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost">
+                <MoreVerticalIcon className="w-4 h-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Ações</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="gap-1.5"
+                onClick={() => {
+                  navigator.clipboard.writeText(idToString);
+                }}
+              >
+                <ClipboardCopyIcon size={16} /> Copiar ID
+              </DropdownMenuItem>
+              <DropdownMenuItem className="gap-1.5">
+                <EditIcon size={16} /> Editar
+              </DropdownMenuItem>
+              <AlertDialogTrigger asChild>
+                <DropdownMenuItem className="gap-1.5">
+                  <TrashIcon size={16} />
+                  Excluir
+                </DropdownMenuItem>
+              </AlertDialogTrigger>
+              <DropdownMenuItem className="gap-1.5">
+                <Info size={16} /> Detalhes
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          {/* Alert content do excluir  */}
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Você está prestes a excluir o produto: {product.name}. Essa ação não pode ser desfeita!
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancelar</AlertDialogCancel>
+              <AlertDialogAction>Excluir</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+          {/* fim do alert content do excluir */}
+        </AlertDialog>
       );
     },
   },
