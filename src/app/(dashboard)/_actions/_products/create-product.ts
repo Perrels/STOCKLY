b803c45e1d7f404dd5fec548010964f9aeaf4e35
Perrels <pablorela@gmail.com>
2024-12-26 21:49:21 -0300

    Add revalidate path to createProduct function

--- src/app/(dashboard)/_actions/_products/create-product.ts
+++ src/app/(dashboard)/_actions/_products/create-product.ts
@@ -1,7 +1,9 @@
 "use server";
 import { db } from "@/app/_lib/prisma";
 import { FormSchemaProduct } from "../../products/_components/add-product-button";
+import { revalidatePath } from "next/cache";
 
 export const createProduct = async (product: FormSchemaProduct) => {
   await db.product.create({ data: product });
+  revalidatePath("/dashboard/products");
 };
