f02307d50751d940dd48ca247cb9f199b8d9e0e4
Perrels <pablorela@gmail.com>
2024-12-27 04:08:17 -0300

    feat add a hard function
    Essa função faz a contabilizaçao na parte de vendas do sistema, foi utilizado useMemo no total de tudo, e também uma complexa função no onSubmit do upsert-sheet-content.tsx

---
+++ src/app/(dashboard)/sales/_components/upsert-sheet-content.tsx
@@ -0,0 +1,206 @@
+"use client";
+import { Combobox, ComboboxOption } from "@/app/_components/combox";
+import { Button } from "@/app/_components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/app/_components/ui/form";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/_components/ui/table";
+import { Input } from "@/app/_components/ui/input";
+import {
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/app/_components/ui/sheet";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Product } from "@prisma/client";
+import { PlusIcon } from "lucide-react";
+import { useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { formatCurrency } from "../../helper/currency";
+
+const formSchema = z.object({
+  productId: z.string(),
+  quantity: z.coerce
+    .number()
+    .min(1, { message: "A quantidade deve ser maior que 0" }),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+interface UpsertSheetContentProps {
+  products: Product[];
+  productOptions: ComboboxOption[];
+}
+
+interface SelectedProducts {
+  // VAI DA PAU TEM QUE SER NUMBER
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+const UpsertSheetContent = ({
+  products,
+  productOptions,
+}: UpsertSheetContentProps) => {
+  const [selectedProducts, setSelectedProducts] = useState<SelectedProducts[]>(
+    []
+  );
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      productId: "",
+      quantity: 1,
+    },
+  });
+
+  const onSubmit = async (data: FormSchema) => {
+    const selectedProduct = products.find(
+      (product) => product.id.toString() === data.productId
+    );
+    if (!selectedProduct) return;
+    // const selectedID = selectedProduct.id.toString();
+    setSelectedProducts((currentyProduct) => {
+      const existingProduct = currentyProduct.find(
+        (product) => product.id === selectedProduct.id
+      );
+      if (existingProduct) {
+        return currentyProduct.map((product) => {
+          if (product.id === selectedProduct.id) {
+            return {
+              ...product,
+              quantity: product.quantity + data.quantity,
+            };
+          }
+          return product;
+        });
+      }
+      return [
+        ...currentyProduct,
+        {
+          ...selectedProduct,
+          price: Number(selectedProduct.price),
+          quantity: data.quantity,
+        },
+      ];
+    });
+    form.reset();
+  };
+
+  const productsTotal = useMemo(() => {
+    return selectedProducts.reduce((acc, product) => {
+      return acc + product.price * product.quantity;
+    }, 0);
+  }, [selectedProducts]);
+
+  return (
+    <>
+      <SheetContent className="!max-w-[50vw] !min-w-[600px]">
+        <SheetHeader>
+          <SheetTitle>Nova venda</SheetTitle>
+          <SheetDescription>
+            Insira as informações da venda abaixo
+          </SheetDescription>
+        </SheetHeader>
+        {/* inicio formulário */}
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 py-6"
+          >
+            <FormField
+              control={form.control}
+              name="productId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Produto</FormLabel>
+                  <FormControl>
+                    <Combobox
+                      placeholder="Selecione um produto"
+                      options={productOptions}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="quantity"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Quantidade</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Insira a quantidade do produto..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button variant={"secondary"} type="submit" className="w-full">
+              <PlusIcon size={20} /> Adicionar produto a venda
+            </Button>
+          </form>
+        </Form>
+        {/* fim do formulário */}
+        {/* inicio da tabela */}
+        <Table>
+          <TableCaption>Lista de produtos selecionados.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Produto</TableHead>
+              <TableHead>Preço Unitário</TableHead>
+              <TableHead>Quantidade</TableHead>
+              <TableHead>Total</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {selectedProducts.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell className="font-medium">{product.id}</TableCell>
+                <TableCell className="font-medium">{product.name}</TableCell>
+                <TableCell>{formatCurrency(product.price)}</TableCell>
+                <TableCell>{product.quantity}</TableCell>
+                <TableCell className="text-right">
+                  {formatCurrency(product.price * product.quantity)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={4}>Total</TableCell>
+              <TableCell className="text-left">{formatCurrency(productsTotal)}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+        {/* fim da tabela */}
+      </SheetContent>
+    </>
+  );
+};
+
+export default UpsertSheetContent;
--- src/app/(dashboard)/sales/page.tsx
+++ src/app/(dashboard)/sales/page.tsx
@@ -1,7 +1,38 @@
-const SalesPage = () => {
+import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet";
+import { Button } from "@/app/_components/ui/button";
+import { PlusIcon } from "lucide-react";
+import UpsertSheetContent from "./_components/upsert-sheet-content";
+import { cachedGetProducts } from "../_actions/_products/products";
+import { ComboboxOption } from "@/app/_components/combox";
+// forçar que a pagina sempre seja renderizada de forma dinamica
+export const dynamic = "force-dynamic";
+
+const SalesPage = async () => {
+  const products = await cachedGetProducts();
+  const productOptions:ComboboxOption[] = products.map((product) => ({
+    label: product.name,
+    value: product.id.toString(),
+  }));
   return (
     <>
-      <h1>Sales Page</h1>
+      <div className="w-full space-y-8 mx-4 mt-3 p-4 bg-white shadow-lg shadow-black/20 rounded-md">
+        <div className="flex w-full items-center justify-between">
+          <div className="space-y-1">
+            <span className="text-xs font-semibold text-slate-500">
+              Gestão de Vendas
+            </span>
+            <h2 className="text-xl font-semibold">Vendas</h2>
+          </div>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button className="gap-2">
+                <PlusIcon size={20} /> Nova venda
+              </Button>
+            </SheetTrigger>
+            <UpsertSheetContent products={products} productOptions={productOptions} />
+          </Sheet>
+        </div>
+      </div>
     </>
   );
 };
